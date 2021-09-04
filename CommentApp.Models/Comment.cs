a44f8cc796f1c1937c7a9c93db64e77b9502fd9b
Gromyko Leonid <Dark-seizer@yandex.ru>
2021-09-04 17:40:33 +0300

    Создание моделей на отзыв и комментарий к отзыву.

---
+++ CommentApp.Models/Comment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommentApp.Models
+{
+    /// <summary>
+    /// Комментарий на чей-то отзыв
+    /// </summary>
+    class Comment
+    {
+        public DateTime CreationTime { get; set; }
+        public string Text { get; set; }
+        public virtual Recall Recall { get; set; }
+    }
+}
---
+++ CommentApp.Models/Recall.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommentApp.Models
+{
+    /// <summary>
+    /// Отзыв на некоторый вид товара, услуги
+    /// </summary>
+    class Recall
+    {
+        public DateTime CreationTime { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public int Grade { get; set; }
+        public virtual List<Comment> Comments { get; set; }
+    }
+}
